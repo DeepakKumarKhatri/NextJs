348a6b1987b37596d1620e93cec07fddfd6baa07
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-04-26 20:24:22 +0500

    Learned private folders routing

---
+++ hello-next/src/app/%5Fappear/page.tsx
@@ -0,0 +1,8 @@
+export default function Experiment() {
+  return (
+    <>
+      <h1>This will appear on the browser because of "%5F".</h1>
+      <p>Can be accessed on URL: http://localhost:3000/_appear</p>
+    </>
+  );
+}
---
+++ hello-next/src/app/_lib/page.tsx
@@ -0,0 +1,7 @@
+export default function Experiment() {
+  return (
+    <>
+      <h1>This will never appear on the browser.</h1>
+    </>
+  );
+}
